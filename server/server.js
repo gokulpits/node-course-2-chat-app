35a57f1ab325dd44dae380bcd971a36f993d6dba
gokul <gokul.gd@pitsolutions.com>
2018-12-06 15:19:37 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+
+const path = require('path');
+
+const express = require('express');
+
+var app = express();
+
+var publicPath = path.join(__dirname,'../public');
+
+var port = process.env.PORT || 3000;
+
+
+app.use(express.static(publicPath));
+
+app.listen(port,()=>{
+    console.log(`Start on port on ${port}`)
+})
